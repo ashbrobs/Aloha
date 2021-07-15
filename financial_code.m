f42a93820ef5d86f661cd81c80e6db4eaa05a10d
2bellr <58318478+2bellr@users.noreply.github.com>
2021-07-15 11:39:51 -0500

    completed basic code

--- financial_code.m
+++ financial_code.m
@@ -5,35 +5,35 @@ mood = 0; % happiness placeholder
 %above are all the variables needed for the code
 
 
-questions = [q1 q2]; %variables array for questions
+questions = [1 2]; %variables array for questions
 
 questions = questions(randperm(length(questions)));
 
 for x = 1:length(questions)
+    a = questions(x);
     if money > 0
         money = money - monthly;
         disp(questions(x));
         % calculate effects using a database of the questions and what they
         % do;; these are placeholders
+        [money, monthly] = qfunc(a, money, monthly);
+        disp("total money:");
+        disp(money);
+        disp("monthly costs:");
+        disp(monthly);
     else
         takeLoan = 0; % testing when user does not take loan
         if takeLoan == 1
             input("Please take out a loan: ")
             monthly = monthly + loan/12;
         else
-            disp("you fail") %this ends all loops
+            disp("you failed at a debt value of"); %this ends all loops
+            disp(money);
             break
         end
     end
 end
 
-function question1 = q1(~)
-    question1 = input("Test1: "); %what I want to do here is,
-    %if q1 is called, I want certain conditions to occur
-    %monthly = monthly + 5; this does not work because monthly is not in
-    %function
-end
-
-function question2 = q2(~)
-    question2 = input("Test2: "); 
-end
+disp("You somehow made it through without losing all of your money!");
+disp("Here is your final money count.");
+disp(money);
